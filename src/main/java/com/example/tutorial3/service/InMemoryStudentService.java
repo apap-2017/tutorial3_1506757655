18b5f5ada8026e694f2ad97247987c533c4476b1
Jessica Susanto <jessciacia1097@gmail.com>
2017-09-23 09:14:21 +0700

    tutor 3

---
+++ src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,42 @@
+package com.example.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.tutorial3.model.StudentModel;
+
+public class InMemoryStudentService implements StudentService {
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+	
+	@Override
+	public StudentModel selectStudent(String npm) {
+		for(int i = 0; i < studentList.size(); i++) {
+			if(npm.equals(studentList.get(i).getNpm())) {
+				return studentList.get(i);
+			}
+		}
+		return null;
+		//ada yang perlu diganti
+	}
+
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		return studentList;
+	}
+
+	@Override
+	public void addStudent(StudentModel student) {
+		studentList.add(student);
+	}
+	
+	@Override
+	public void deleteStudent (String npm) {
+		StudentModel student = null;
+		for(int i = 0; i < studentList.size(); i++) {
+			if(npm.equals(studentList.get(i).getNpm())) {
+				student = studentList.get(i);
+			}
+		}
+		studentList.remove(student);
+	}
+
+}
